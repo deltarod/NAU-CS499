340164f702c556eb198abbda2bf8ec1d87a92529
Tristan Miller <tem83@nau.edu>
2020-02-20 16:46:01 -0700

    Started Work on KFold as well as Main Runner

--- Project2/KFoldCV.R
+++ Project2/KFoldCV.R
@@ -5,5 +5,28 @@
 
 KFoldCV <- function( X_mat, y_vec, ComputePredictions, fold_vec )
 {
+    if( length( fold_vec ) != length( y_vec ) )
+    {
+        print("fold_vec and y_vec are not the same size")
+
+        return()
+    }
+
+    error_vec <- rep(0.0, length(fold_vec))
+
+    for( k in fold_vec )
+    {
+        X_new
+
+        y_new
+
+        pred_new <- ComputePredictions(X_train, y_train, X_new)
+    }
+
+    return(error_vec)
+}
+
+PredictionWrapper <- function( X_train, y_train, X_new )
+{
 
 }
--- Project2/MainRunner.R
+++ Project2/MainRunner.R
@@ -3,3 +3,39 @@
 # Created by: Tristan Miller (tem83)
 # Created on: 2/20/2020
 
+#check for package installation
+if( !require( "data.table" ) ){
+    install.packages( "data.table" )
+}
+
+#load packages
+library( data.table )
+library( ggplot2 )
+source("Project2/KFoldCV.R")
+
+#acquire data
+if(!file.exists( "spam.data" ) ){
+    download.file( "https://web.stanford.edu/~hastie/ElemStatLearn/datasets/spam.data", "spam.data" )
+}
+
+spam.dt <- data.table::fread( "spam.data" )
+
+observation_size <- nrow( spam.dt )
+
+#-ncol(spam.dt) because the output is on the last col
+data <- as.matrix( spam.dt[ , -ncol( spam.dt ), with=FALSE ] )
+
+#seperates the outputs
+outputs <- spam.dt[[ncol( spam.dt )]]
+
+data.scaled <- scale( data )
+
+#reproducable tests
+set.seed(1 )
+
+numFolds <- 10
+
+fold_vec <- sample( rep( 1:numFolds, l=nrow( data.scaled ) ) )
+
+output <- KFoldCV( data.scaled, outputs, PredictionWrapper(), fold_vec )
+
