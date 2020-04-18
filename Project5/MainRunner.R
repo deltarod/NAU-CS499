1c0e685d45411bd71b331ae13f79adbceb0d7bf7
Tristan Miller <tem83@nau.edu>
2020-04-17 20:21:13 -0700

    Finished Project5

--- Project5/MainRunner.R
+++ Project5/MainRunner.R
@@ -3,30 +3,15 @@
 # Created by: Tristan Miller (tem83)
 # Created on: 4/13/2020
 
-#load and install deps
-if( !require( "tensorflow" ) ){
-    install.packages( "tensorflow" )
-
-}
-
-if( !require( "keras" ) ){
-    install.packages( "keras" )
-    keras::install_keras(version="2.2.4")
-}
+# tensorflow::install_tensorflow(version="2.1")
 
 library( keras )
+library( tensorflow )
 library( data.table )
 library( ggplot2 )
 
-# Uncomment this if nvidia graphics card
-#install_tensorflow( version = "nightly-gpu" )
-
-# Uncomment this otherwise
-# install_tensorflow()
-
-
 #import code for training model
-#source("Project5/Model.R")
+source("Project5/Model.R")
 
 #acquire data
 if(!file.exists("spam.data") ){
@@ -67,20 +52,147 @@ spam.x.validation <- spam.x.train[spam.validation, ]
 spam.y.subtrain <- spam.y.train[ spam.subtrain ]
 spam.y.validation <- spam.y.train[ spam.validation ]
 
-model <- keras_model_sequential() %>%
-    layer_flatten( input_shape = ncol( spam.x.subtrain ) ) %>%
-    layer_dense( units = 5, activation = "sigmoid", use_bias = FALSE ) %>%
-    layer_dense(1, activation = "sigmoid", use_bias = FALSE )
 
-model %>%
-    compile(
-        loss = "binary_crossentropy",
-        optimizer = "adam",
-        metrics = "accuracy"
-    )
-model %>%
-    fit(x=spam.x.subtrain, y=spam.y.subtrain,
-        epochs=5,
-        validation_data = list(spam.x.validation, spam.y.validation))
+######################### Hidden Unit Training #########################
 
-print("Test2")
+#train on up to 5 hidden units
+hiddenUnits.output <- RunModel(spam.x.subtrain, spam.y.subtrain, list( spam.x.validation, spam.y.validation ), hiddenUnits = 5, hiddenUnits.train = TRUE )
+
+#graph outputs
+hiddenUnits.graph <- ggplot()+
+    geom_line( aes( x = epoch, y = loss, color = factor( hiddenUnits ) ), linetype = 1, data = hiddenUnits.output[[1]] )+
+    geom_line( aes( x = epoch, y = val_loss, color = factor( hiddenUnits ) ), linetype = 2, data = hiddenUnits.output[[1]])+
+    geom_point( aes( x = epoch, y = val_loss, color = factor( hiddenUnits ) ), data = hiddenUnits.output[[2]] )+
+    labs( color = "Number Of Hidden Units" )+
+    ggtitle( "Training/Validation Loss of Number of Hidden Units")
+
+#determine best parameter
+hiddenUnits.best_parameter <- hiddenUnits.output[[2]][which.min(val_loss)]
+
+hiddenUnits.best_parameter_value <- hiddenUnits.best_parameter[ 1, hiddenUnits.best_parameter$hiddenUnits ]
+
+hiddenUnits.best_epoch <- hiddenUnits.best_parameter[ 1, hiddenUnits.best_parameter$epoch ]
+
+#retrain
+hiddenUnits.retrain.units <- RunModel( spam.x.train, spam.y.train, NULL, epochs = hiddenUnits.best_epoch, hiddenUnits = hiddenUnits.best_parameter_value, subtrain = FALSE )
+
+hiddenUnits.retrain.eval.units <- EvaluateModel( hiddenUnits.retrain.units[[2]], spam.x.test, spam.y.test )
+
+sprintf( "Hidden Units Trained Accuracy: %f", hiddenUnits.retrain.eval.units[[2]] )
+
+######################### Hidden Layer Training #########################
+
+hiddenLayers.output <- RunModel(spam.x.subtrain, spam.y.subtrain, list( spam.x.validation, spam.y.validation ), hiddenLayers = 5, hiddenLayers.train = TRUE )
+
+#graph outputs
+hiddenLayers.graph <- ggplot()+
+    geom_line( aes( x = epoch, y = loss, color = factor( hiddenLayers ) ), linetype = 1, data = hiddenLayers.output[[1]] )+
+    geom_line( aes( x = epoch, y = val_loss, color = factor( hiddenLayers ) ), linetype = 2, data = hiddenLayers.output[[1]])+
+    geom_point( aes( x = epoch, y = val_loss, color = factor( hiddenLayers ) ), data = hiddenLayers.output[[2]] )+
+    labs( color = "Number Of Hidden Layers" )+
+    ggtitle( "Training/Validation Loss of Number of Hidden Layers")
+
+#determine best parameter
+hiddenLayers.best_parameter <- hiddenLayers.output[[2]][which.min(val_loss)]
+
+hiddenLayers.best_parameter_value <- hiddenLayers.best_parameter[ 1, hiddenLayers.best_parameter$hiddenUnits ]
+
+hiddenLayers.best_epoch <- hiddenLayers.best_parameter[ 1, hiddenLayers.best_parameter$epoch ]
+
+#retrain
+hiddenLayers.retrain <- RunModel( spam.x.train, spam.y.train, NULL, epochs = hiddenLayers.best_epoch, hiddenUnits = hiddenLayers.best_parameter_value, subtrain = FALSE )
+
+hiddenLayers.retrain.eval <- EvaluateModel( hiddenLayers.retrain[[2]], spam.x.test, spam.y.test )
+
+sprintf( "Hidden Layers Trained Accuracy: %f", hiddenLayers.retrain.eval[[2]] )
+
+
+######################### Degree of L2/weight decay Training #########################
+
+l2.output <- RunModel(spam.x.subtrain, spam.y.subtrain, list( spam.x.validation, spam.y.validation ), l2 = 0.01, l2.train = TRUE )
+
+#graph outputs
+l2.graph <- ggplot()+
+    geom_line( aes( x = epoch, y = loss, color = factor( l2 ) ), linetype = 1, data = l2.output[[1]] )+
+    geom_line( aes( x = epoch, y = val_loss, color = factor( l2 ) ), linetype = 2, data = l2.output[[1]])+
+    geom_point( aes( x = epoch, y = val_loss, color = factor( l2 ) ), data = l2.output[[2]] )+
+    labs( color = "L2 Value" )+
+    ggtitle( "Training/Validation Loss of L2 Values")
+
+#determine best parameter
+l2.best_parameter <- l2.output[[2]][which.min(val_loss)]
+
+l2.best_parameter_value <- l2.best_parameter[ 1, l2.best_parameter$l2 ]
+
+l2.best_epoch <- l2.best_parameter[ 1, l2.best_parameter$epoch ]
+
+#retrain
+l2.retrain <- RunModel( spam.x.train, spam.y.train, NULL, epochs = l2.best_epoch, l2 = l2.best_parameter_value, subtrain = FALSE )
+
+l2.retrain.eval <- EvaluateModel( l2.retrain[[2]], spam.x.test, spam.y.test )
+
+sprintf( "L2 Trained Accuracy: %f", l2.retrain.eval[[2]] )
+
+######################### Degree of Noise Training #########################
+
+noise.output <- RunModel(spam.x.subtrain, spam.y.subtrain, list( spam.x.validation, spam.y.validation ), noise = 0.4, noise.train = TRUE )
+
+#graph outputs
+noise.graph <- ggplot()+
+    geom_line( aes( x = epoch, y = loss, color = factor( noise ) ), linetype = 1, data = noise.output[[1]] )+
+    geom_line( aes( x = epoch, y = val_loss, color = factor( noise ) ), linetype = 2, data = noise.output[[1]])+
+    geom_point( aes( x = epoch, y = val_loss, color = factor( noise ) ), data = noise.output[[2]] )+
+    labs( color = "Noise Value" )+
+    ggtitle( "Training/Validation Loss of Noise Values")
+
+#determine best parameter
+noise.best_parameter <- noise.output[[2]][which.min(val_loss)]
+
+noise.best_parameter_value <- noise.best_parameter[ 1, noise.best_parameter$noise ]
+
+noise.best_epoch <- noise.best_parameter[ 1, noise.best_parameter$epoch ]
+
+#retrain
+noise.retrain <- RunModel( spam.x.train, spam.y.train, NULL, epochs = noise.best_epoch, noise = noise.best_parameter_value, subtrain = FALSE )
+
+noise.retrain.eval <- EvaluateModel( noise.retrain[[2]], spam.x.test, spam.y.test )
+
+sprintf( "Noise Trained Accuracy: %f", noise.retrain.eval[[2]] )
+
+
+######################### Degree of DropOut Training #########################
+
+dropOut.output <- RunModel(spam.x.subtrain, spam.y.subtrain, list( spam.x.validation, spam.y.validation ), dropOut = 0.4, dropOut.train = TRUE )
+
+#graph outputs
+dropOut.graph <- ggplot()+
+    geom_line( aes( x = epoch, y = loss, color = factor( dropOut ) ), linetype = 1, data = dropOut.output[[1]] )+
+    geom_line( aes( x = epoch, y = val_loss, color = factor( dropOut ) ), linetype = 2, data = dropOut.output[[1]])+
+    geom_point( aes( x = epoch, y = val_loss, color = factor( dropOut ) ), data = dropOut.output[[2]] )+
+    labs( color = "Drop Out Value" )+
+    ggtitle( "Training/Validation Loss of Drop Out Values")
+
+#determine best parameter
+dropOut.best_parameter <- dropOut.output[[2]][which.min(val_loss)]
+
+dropOut.best_parameter_value <- dropOut.best_parameter[ 1, dropOut.best_parameter$noise ]
+
+dropOut.best_epoch <- dropOut.best_parameter[ 1, dropOut.best_parameter$epoch ]
+
+#retrain
+dropOut.retrain <- RunModel( spam.x.train, spam.y.train, NULL, epochs = dropOut.best_epoch, dropOut = dropOut.best_parameter_value, subtrain = FALSE )
+
+dropOut.retrain.eval <- EvaluateModel( dropOut.retrain[[2]], spam.x.test, spam.y.test )
+
+sprintf( "Drop Out Trained Accuracy: %f", dropOut.retrain.eval[[2]] )
+
+
+#determine baseline accuracy
+baselineAccuracy <- BaselineAccuracy( spam.y.test )
+sprintf( "Baseline Accuracy: %f", baselineAccuracy )
+
+ggsave("Project5/HiddenUnits.png", plot = hiddenUnits.graph)
+ggsave("Project5/HiddenLayers.png", plot = hiddenLayers.graph)
+ggsave("Project5/l2.png", plot = l2.graph)
+ggsave("Project5/Noise.png", plot = noise.graph)
+ggsave("Project5/DropOut.png", plot = dropOut.graph)
--- Project5/Model.R
+++ Project5/Model.R
@@ -3,70 +3,155 @@
 # Created by: Tristan Miller (tem83)
 # Created on: 4/3/2020
 
-GenerateModel <- function( hiddenUnits, data.x, epochs )
+
+RunModel <- function( train.x, train.y,
+                      validation,
+                      hiddenUnits = 5, hiddenUnits.train = FALSE,
+                      hiddenLayers = 3, hiddenLayers.train = FALSE,
+                      l2 = 0, l2.train = FALSE,
+                      noise = 0, noise.train = FALSE,
+                      dropOut = 0, dropOut.train = FALSE,
+                      epochs = 50,
+                      subtrain = TRUE )
+{
+    if( subtrain )
     {
+        #check if training hiddenUnits
+        if( hiddenUnits.train )
+        {
+            regularization <- 1:hiddenUnits
+        }
 
-    #setup the model
-    model <- keras_model_sequential() %>%
-        layer_flatten( input_shape = ncol( data.x ) ) %>%
-        layer_dense( units = hiddenUnits, activation = "sigmoid", use_bias = FALSE ) %>%
-        layer_dense(1, activation = "sigmoid", use_bias = FALSE )
+        #check if training hiddenLayers
+        if( hiddenLayers.train )
+        {
+            regularization <- 1:hiddenLayers
+        }
 
-    #compile the model with required functions and metrix
-    model %>%
-        compile(
-            loss = "binary_crossentropy",
-            optimizer = "adam",
-            metrics = "accuracy"
-        )
+        if( l2.train )
+        {
+            regularization <- seq(0, l2, 0.002)
+        }
 
-    return(model)
+        if( noise.train )
+        {
+            regularization <- seq(0, noise, 0.08)
+        }
+
+        if( dropOut.train )
+        {
+            regularization <- seq(0, dropOut, 0.08)
+        }
+
+        min.dt <- data.table()
+    }
+    else
+    {
+        regularization <- 1
+    }
+
+    info.dt <- data.table()
+
+    for( regular in regularization )
+    {
+        #setup the model
+        model <- keras_model_sequential()
+
+        if(noise.train)
+        {
+            layer_gaussian_noise(model, noise )
+        }
+        if(dropOut.train)
+        {
+            layer_gaussian_dropout( model, dropOut )
+        }
+
+        #loop to setup number of hidden layers
+        for( k in {if( hiddenLayers.train ) 1:regular else 1:hiddenLayers} )
+        {
+            layer_dense( model,
+                         input_shape = ncol(train.x),
+                         units = { if( hiddenUnits.train ) regular else hiddenUnits },
+                         activation = "sigmoid",
+                         kernel_regularizer=regularizer_l2(l = { if( l2.train ) regular else l2 } ),
+                         use_bias = FALSE )
+        }
+
+        layer_dense(model,1, activation = "sigmoid", use_bias = FALSE ) #output layer
+
+        #compile the model with required functions and metrix
+        model %>%
+            compile(
+                loss = "binary_crossentropy",
+                optimizer = optimizer_adam(lr=0.01),
+                metrics = "accuracy"
+            )
+
+        if( subtrain )
+            {
+            result <- model %>%
+                fit(
+                    x = train.x,
+                    y = train.y,
+                    epochs = epochs,
+                    validation_data = validation,
+                    verbose = 2
+                )
+        }
+        else
+            {
+            result <- model %>%
+                fit(
+                    x = train.x,
+                    y = train.y,
+                    epochs = epochs,
+                    validation_split = 0,
+                    verbose = 2
+                )
+        }
+
+        modelInfo <- do.call( data.table::data.table, result$metrics )
+        modelInfo[, hiddenUnits := { if(hiddenUnits.train) regular else hiddenUnits }  ]
+        modelInfo[, hiddenLayers := { if( hiddenLayers.train) regular else hiddenLayers } ]
+        modelInfo[, l2 := { if( l2.train) regular else l2 } ]
+        modelInfo[, noise := { if( noise.train) regular else noise } ]
+        modelInfo[, dropOut := { if( dropOut.train) regular else dropOut } ]
+        modelInfo[, epoch := 1:epochs ]
+
+        if( subtrain )
+        {
+            min.dt <- rbind( min.dt, modelInfo[which.min(val_loss)] )
+        }
+
+        info.dt <- rbind( info.dt, modelInfo )
+
+    }
+
+    if( subtrain )
+    {
+        return( list( info.dt, min.dt ) )
+    }
+    else
+    {
+        return( list( info.dt, model ) )
+    }
+
+
 }
 
-TrainModel <- function( model, data.x, data.y, epochs, validation_split = 0.4 )
+EvaluateModel <- function( model, x, y )
 {
-#begin training the model
-    result <- model %>%
-        fit(
-        x = data.x,
-        y = data.y,
-        epochs = epochs,
-        validation_split = validation_split,
-        verbose = 2
-        )
-
-    return(result)
+    return( evaluate( model, x, y ) )
 }
 
-GenerateModelInfo <- function( result, hiddenUnits, epochs )
+BaselineAccuracy <- function( y )
 {
-    modelInfo <- do.call( data.table::data.table, result$metrics )
-    modelInfo[, hiddenUnits := hiddenUnits ]
-    modelInfo[, epoch := 1:epochs ]
+    y.table <- table( y )
+    baseline <- { if( y.table[1] > y.table[2] ) 0 else 1 }
+    baseline.accuracy <- mean( y == baseline )
 }
 
 
-RunModel <- function(x, y, validation, hiddenUnits = 5, epochs = 50 )
-{
-    model <- keras_model_sequential() %>%
-        layer_flatten( input_shape = ncol( x ) ) %>%
-        layer_dense( units = hiddenUnits, activation = "sigmoid", use_bias = FALSE ) %>%
-        layer_dense(1, activation = "sigmoid", use_bias = FALSE )
-
-    model %>%
-        compile(
-            loss = "binary_crossentropy",
-            optimizer = "adam",
-            metrics = "accuracy"
-        )
-    result <- model %>%
-        fit(x=data.table(x), y=data.table(y),
-            epochs=50,
-            validation_data = validation,
-            verbose=0)
-
-    return(list(model, result))
-}
 
 
 
